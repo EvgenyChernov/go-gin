030c925de1907140bb701e26a7e89fd6defbeb54
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-08 12:42:49 +0300

    создание конфигов

--- auth/main.go
+++ auth/main.go
@@ -1,16 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"auth/internal/config"
+	"fmt"
+)
 
 func main() {
-	// Создаем новый роутер Gin
-	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Привет я Gin!",
-		})
-	})
+	// Инициализируем конфигурации сервера
+	cfg := config.NewConfig()
 
-	router.Run(":8101")
+	fmt.Printf("=== Server Configuration ===\n")
+	fmt.Printf("Host: %s\n", cfg.Host)
+	fmt.Printf("Port: %s\n", cfg.Port)
+	fmt.Printf("Database URL: %s\n", cfg.DBDSN)
+	fmt.Printf("Access Token Expiration: %d hours\n", cfg.AccessTokenExpiration)
+	fmt.Printf("Refresh Token Expiration: %d hours\n", cfg.RefreshTokenExpiration)
+	fmt.Printf("Server Timeout: %d seconds\n", cfg.Timeout)
+	fmt.Printf("Database Timeout: %d seconds\n", cfg.DBTimeout)
+	fmt.Printf("JWT Secret Key: %s\n", cfg.JWTSecretKey)
+	fmt.Printf("=============================\n")
+
+	// // Создаем новый роутер Gin
+	// router := gin.Default()
+
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "Привет я Gin!",
+	// 	})
+	// })
+
+	// router.Run(":8101")
 }
