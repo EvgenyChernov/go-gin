48e9a171c65e3d4b5a3e4d047e48e9bd40fe036a
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-08 19:18:47 +0300

    Добавлен новый сервис 'db_backup' в docker-compose для автоматического резервного копирования базы данных, включая конфигурацию расписания и зависимости от сервиса 'db_notes'.

---
+++ notes/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"notes/internal/config"
+)
+
+func main() {
+	cfg := config.NewConfig()
+
+	fmt.Printf("=== notes Server Configuration ===\n")
+	fmt.Printf("Host: %s\n", cfg.Host)
+	fmt.Printf("Port: %s\n", cfg.Port)
+	fmt.Printf("Database URL: %s\n", cfg.DBDSN)
+	fmt.Printf("Database SSL: %s\n", cfg.DBSSL)
+	fmt.Printf("Server Timeout: %d seconds\n", cfg.Timeout)
+	fmt.Printf("Database Timeout: %d seconds\n", cfg.DBTimeout)
+	fmt.Printf("Redis Host: %s\n", cfg.RedisHost)
+	fmt.Printf("Redis Port: %s\n", cfg.RedisPort)
+	fmt.Printf("Redis Password: %s\n", cfg.RedisPassword)
+	fmt.Printf("JWT Secret Key: %s\n", cfg.JWTSecretKey)
+	fmt.Printf("Database Name: %s\n", cfg.DB_NAME)
+	fmt.Printf("Database Collection: %s\n", cfg.DB_COLLECTION)
+	fmt.Printf("=============================\n")
+}
