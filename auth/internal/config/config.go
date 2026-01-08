83c97033598b67360f15481bd015dd1985b74c10
evgeny.chernov <EvgenyCher89@gmail.com>
2026-01-08 14:11:50 +0300

    Создание покета для работы с базой данных

--- auth/internal/config/config.go
+++ auth/internal/config/config.go
@@ -11,6 +11,7 @@ type Config struct {
 	Host                   string // Хост, на котором будет запущен сервер
 	Timeout                int    // Таймаут для операций с сервером в секундах
 	DBDSN                  string // Строка подключения к базе данных, например, "postgres://user:password@localhost:5432/dbname"
+	DBSSL                  string // Режим SSL для базы данных
 	DBTimeout              int    // Таймаут для операций с базой данных в секундах
 	JWTSecretKey           string // Секретный ключ для JWT токенов
 	AccessTokenExpiration  int    // Срок действия access токена в часах
@@ -58,8 +59,33 @@ func NewConfig() *Config {
 		fmt.Println("Не удалось получить DB_TIMEOUT из переменной окружения, используется 5 секунд")
 	}
 
+	dbHost, err := getEnv("POSTGRES_HOST")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_HOST из переменной окружения")
+	}
+	dbPort, err := getEnv("POSTGRES_PORT")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_PORT из переменной окружения")
+	}
+	dbUser, err := getEnv("POSTGRES_USER")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_USER из переменной окружения")
+	}
+	dbPassword, err := getEnv("POSTGRES_PASSWORD")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_PASSWORD из переменной окружения")
+	}
+	dbName, err := getEnv("POSTGRES_DB")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_DB из переменной окружения")
+	}
+	dbSSL, err := getEnv("POSTGRES_USE_SSL")
+	if err != nil {
+		fmt.Println("Не удалось получить POSTGRES_USE_SSL из переменной окружения")
+	}
 	// Формирование строки подключения к базе данных
-	dbDSN := "Пока не реализовано" // Здесь должна быть логика формирования строки подключения к базе данных
+	dbDSN := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbSSL)
 
 	// JWT настройки
 	jwtSecretKey, err := getEnv("JWT_SECRET_KEY")
@@ -85,6 +111,7 @@ func NewConfig() *Config {
 		Port:                   port,
 		Host:                   host,
 		DBDSN:                  dbDSN, // Строка подключения к базе данных
+		DBSSL:                  dbSSL,
 		JWTSecretKey:           jwtSecretKey,
 		AccessTokenExpiration:  accessTokenExpiration,
 		RefreshTokenExpiration: refreshTokenExpiration,
